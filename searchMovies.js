6b513406fd5a5d088e85d09ddbec49fc9d7a6070
Rutvi Shah <32719043+rutvishah859@users.noreply.github.com>
2020-12-23 16:45:24 -0500

    Search requests to the API
    The user can search movies on the site and the movies will be listed in the results div box

--- searchMovies.js
+++ searchMovies.js
@@ -1,8 +1,29 @@
 function searchMovie(){
     var movie = document.getElementById("movieSearch").value; 
     var results = document.getElementById("results");
-    var searchResults = new Array();
-    //request = new Request('https://www.omdbapi.com/?apikey=4dd3fc90&s=' + title);
+    results.innerHTML = ""; //each time the user makes a request reset the request box
+
+    //request to get the info about the movie searched by the user from the API 
+    var request = new Request('http://www.omdbapi.com/?s=' + movie + '&apikey=fceeaa45');
+
+    var searchResults = document.createElement('h3');
+    searchResults.innerHTML = "Results for \"" + movie + "\"";
+    results.appendChild(searchResults);
+
+    fetch(request).then((response) => {
+        return response.json();
+      }).then(function(data){
+        console.log(data);
+        for (var i = 0; i < data.Search.length; i++){
+            //only print the movies (exclude the search results that are TV shows and etc.)
+            if (data.Search[i].Type == 'movie'){
+                var li = document.createElement('li'); 
+                li.textContent = data.Search[i].Title + " (" + data.Search[i].Year + ")";
+                results.appendChild(li);
+            }  
+        }
+      });
+
     //console.log(movie);
     //results.innerHTML = movie;
 }
