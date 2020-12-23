e210414731513b884463a3a35f64d1a204d7c106
Rutvi Shah <32719043+rutvishah859@users.noreply.github.com>
2020-12-23 17:53:34 -0500

    Append moives into the nomination list
    When the button to nominate the movie is pressed the movie is added to the nomination list

--- searchMovies.js
+++ searchMovies.js
@@ -17,13 +17,35 @@ function searchMovie(){
         for (var i = 0; i < data.Search.length; i++){
             //only print the movies (exclude the search results that are TV shows and etc.)
             if (data.Search[i].Type == 'movie'){
-                var li = document.createElement('li'); 
-                li.textContent = data.Search[i].Title + " (" + data.Search[i].Year + ")";
-                results.appendChild(li);
+                //create an unordered list element
+                var movieList = document.createElement('li'); 
+                //create a button which will nominate the moive 
+                var nominateButton = document.createElement('button');
+                nominateButton.innerHTML = "Nominate";
+                nominateButton.value = data.Search[i].imdbID; 
+
+                //when the nominate button is clicked add that moive into the nominations
+                nominateButton.addEventListener('click', function(e){ 
+                    var nominations = document.getElementById("nominations");
+                    //create an unordered list
+                    var nominationList = document.createElement('li');
+
+                    var index; //hold the movie index (data.Search[index])
+                    //Note: e.target.value gives the value of the button that was clicked
+                    for (var a = 0; a < data.Search.length; a++){
+                        if (data.Search[a].imdbID == e.target.value){
+                            index = a;
+                        }
+                    }
+                    nominationList.textContent = data.Search[index].Title + " (" + data.Search[index].Year + ") ";
+                    nominations.appendChild(nominationList); //append the movie into the nomination list
+                    e.target.disabled = true; //disable the button when the moive has been selected to nominated
+                });
+
+                movieList.textContent = data.Search[i].Title + " (" + data.Search[i].Year + ") ";
+                movieList.appendChild(nominateButton);
+                results.appendChild(movieList);
             }  
         }
       });
-
-    //console.log(movie);
-    //results.innerHTML = movie;
 }
