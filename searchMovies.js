454d84b4d6692e84a4dfe1656214289a989aed79
Rutvi Shah <32719043+rutvishah859@users.noreply.github.com>
2020-12-22 23:20:58 -0500

    worked on the search bar, and stated on the js code

---
+++ searchMovies.js
@@ -0,0 +1,8 @@
+function searchMovie(){
+    var movie = document.getElementById("movieSearch").value; 
+    var results = document.getElementById("results");
+    var searchResults = new Array();
+    //request = new Request('https://www.omdbapi.com/?apikey=4dd3fc90&s=' + title);
+    //console.log(movie);
+    //results.innerHTML = movie;
+}
