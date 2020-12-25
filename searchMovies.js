5735c06c7baf9a2fa33b940bbed0ecdc990ac8c0
Rutvi Shah <32719043+rutvishah859@users.noreply.github.com>
2020-12-25 14:45:28 -0500

    Final Code
    The user is now able to select movies to put into or take out of their nominations list. When the user makes 5 nominations and trys to makes more selections they are given an alert, and all the nomination buttons are disabled. When the user removes nominations then they are again allowed to make more selections.

--- searchMovies.js
+++ searchMovies.js
@@ -1,6 +1,10 @@
+var numNominated = 0; //tracks the num of nominations made (when the nominations exceed 5 the user is no longer allowed to make anymore selections)
+var disabled = false; //boolean value that stoes wheather or not the nomination buttons have been disabled 
+
 function searchMovie(){
     var movie = document.getElementById("movieSearch").value; 
     var results = document.getElementById("results");
+    var nominations = document.getElementById("nominations");
     results.innerHTML = ""; //each time the user makes a request reset the request box
 
     //request to get the info about the movie searched by the user from the API 
@@ -10,14 +14,16 @@ function searchMovie(){
     searchResults.innerHTML = "Results for \"" + movie + "\"";
     results.appendChild(searchResults);
 
+    var nominationsText = document.createElement('h3');
+    nominationsText.innerHTML = "Nominations";
+    nominations.appendChild(nominationsText);
+
     fetch(request).then((response) => {
         return response.json();
       }).then(function(data){
-        console.log(data);
         for (var i = 0; i < data.Search.length; i++){
             //only print the movies (exclude the search results that are TV shows and etc.)
             if (data.Search[i].Type == 'movie'){
-                //var numNominated = 0;
                 //create an unordered list element
                 var movieList = document.createElement('li'); 
                 //create a button which will nominate the moive 
@@ -32,16 +38,18 @@ function searchMovie(){
 
                 //when the nominate button is clicked add that moive into the nominations
                 nominateButton.addEventListener('click', function(e){ 
-                    //numNominated++;
-                    /*if (numNominated > 5){
+                    //the the num of nominations exceed 5 then disbale all the nomination buttons
+                    if (numNominated >= 5){
                         disableAll();
+                        disabled = true;
                     }
-                    else{*/
-                        var nominations = document.getElementById("nominations");
+                    else{
+                        numNominated++;
                         //create an unordered list
                         var nominationList = document.createElement('li');
                         var removeButton = document.createElement('button');
                         removeButton.innerHTML = "Remove";
+                        removeButton.name = "remove";
                         removeButton.value = e.target.value;
 
                         var index; //hold the movie index (data.Search[index])
@@ -58,6 +66,12 @@ function searchMovie(){
 
                         //the remove button removes the nominated moive 
                         removeButton.addEventListener('click', function(e){
+                            numNominated--;
+                            //if all of the buttons are disabled (there are already 5 nominations) then enable all the buttons again so that the user can make new nominations
+                            if (disabled){
+                                enableAll();
+                                disabled = false;
+                            }
                             nominationList.remove(nominationList.childNodes); //removes both the movie title and remove button associated with the moive
                             var nominateBtns = document.getElementsByName("nominate");
 
@@ -68,7 +82,7 @@ function searchMovie(){
                                 }
                             }     
                         });
-                    //}  
+                    }  
                 });                
             }  
         }
@@ -82,4 +96,22 @@ function disableAll(){
         nominations[i].disabled = true; 
     }
     alert("You have made 5 nominations, you cannot make anymore!");
+}
+
+//when the buttons have been disabled, but the user removes a nomination from the nomination list re-enable the buttons
+function enableAll(){
+    var nominations = document.getElementsByName("nominate");
+    var removeBtns = document.getElementsByName('remove');
+
+    for(var i = 0; i < nominations.length; i++){
+        nominations[i].disabled = false;
+    }
+    for (var i = 0; i < removeBtns.length; i++){
+        for (var a = 0; a < nominations.length; a++){
+            if (nominations[a].value == removeBtns[i].value){
+                nominations[a].disabled = true;
+                continue;
+            }
+        }
+    }
 }
